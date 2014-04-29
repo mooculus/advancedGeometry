00603b8680a71364de181ce38ceb717a9cc1408f
Bart Snapp <Bart Snapp>
2014-04-29 14:39:04 -0400

    working on this

--- euclidsPostulatesForPlaneGeometry/ssa.js
+++ euclidsPostulatesForPlaneGeometry/ssa.js
@@ -1,64 +1,143 @@
+// here we set the radius of the points 
+var radius = new Size(5); // global radius of points
+
+var space = new Point(400,0); // distance between triangles in interactive
+
+
 // place the vertices of triangle1
-var pointA1 = new Point(50, 200); 
-var pointB1 = new Point(300, 200);
-var pointC1 = new Point(150, 50);
-var space = new Point(400,0);
+var pointA1 = new Point(150, 50);
+var pointB1 = new Point(50, 200); 
+var pointC1 = new Point(300, 200);
+
 
 // place the vertices of triangle2
 var pointA2 = pointA1 + space; 
 var pointB2 = pointB1 + space; 
 var pointC2 = pointC1 + space; 
 
-// draw the sides of triangle1
-var triangle1 = new Path();
-triangle1.add(pointA1); 
-triangle1.add(pointB1); 
-triangle1.add(pointC1);
-triangle1.closed = true;
-triangle1.style = {
+
+var vectorA1B1 = pointB1-pointA1;
+var normalVectorA1B1 = vectorA1B1.normalize();
+var orthonormalVectorA1B1 = normalVectorA1B1.rotate(90);
+
+var midA1B1 = (pointA1+pointB1)/2;
+
+var hash1 = new Path({
+    segments: [midA1B1-orthonormalVectorA1B1*10, midA1B1+orthonormalVectorA1B1*10],
+    strokeColor: 'black'
+});
+
+// var circleT1 = new Path.Circle({
+//     center: midA1B1,
+//     radius: radius,
+//     fillColor: 'black'
+// });
+
+
+
+
+// draw the solid sides of triangle1
+var triangle1 = new Path({
+    segments: [pointA1,pointB1,pointC1],
     strokeColor: 'black',
     strokeWidth: 3,
     strokeJoin: 'round' // removes unsightly pointies 
-};
+});
 
-// draw the sides of triangle2
-var triangle2 = new Path();
-triangle2.add(pointA2); 
-triangle2.add(pointB2); 
-triangle2.add(pointC2);
-triangle2.closed = true;
-triangle2.style = {
+// draw the solid sides of triangle2
+var triangle2 = new Path({
+    segments: [pointA2,pointB2,pointC2],
     strokeColor: 'black',
     strokeWidth: 3,
     strokeJoin: 'round' // removes unsightly pointies 
-};
+});
 
-// here we set the diameter of the points 
-var diameter = new Size(5);
+// draw the sides of the farside of triangle1
+var farside1 = new Path({
+    segments: [pointC1,pointA1],
+    strokeColor: 'black',
+    strokeWidth: 3,
+    strokeJoin: 'round', // removes unsightly pointies 
+});
+farside1.dashArray = [10,4];
+
+// draw the sides of the farside of triangle2
+var farside2 = new Path({
+    segments: [pointC2,pointA2],
+    strokeColor: 'black',
+    strokeWidth: 3,
+    strokeJoin: 'round', // removes unsightly pointies 
+});
+farside2.dashArray = [10,4];
 
 // now we make circles at each vertex
-var circleA1 = new Path.Circle(pointA1, diameter);
-circleA1.fillColor = 'blue';
+var circleA1 = new Path.Circle({
+    center: pointA1, 
+    radius: radius,
+    fillColor: 'blue'
+});
 
-var circleA2 = new Path.Circle(pointA2, diameter);
-circleA2.fillColor = 'blue';
+var circleA2 = new Path.Circle({
+    center: pointA2, 
+    radius: radius,
+    fillColor: 'blue'
+});
 
-var circleB1 = new Path.Circle(pointB1, diameter);
-circleB1.fillColor = 'red';
+var circleB1 = new Path.Circle({
+    center: pointB1, 
+    radius: radius,
+    fillColor: 'red'
+});
 
-var circleB2 = new Path.Circle(pointB2, diameter);
-circleB2.fillColor = 'red';
+var circleB2 = new Path.Circle({
+    center: pointB2, 
+    radius: radius,
+    fillColor: 'red'
+});
 
-var circleC1 = new Path.Circle(pointC1, diameter);
-circleC1.fillColor = 'green';
+var circleC1 = new Path.Circle({
+    center: pointC1, 
+    radius: radius,
+    fillColor: 'green'
+});
 
-var circleC2 = new Path.Circle(pointC2, diameter);
-circleC2.fillColor = 'green';
+var circleC2 = new Path.Circle({
+    center: pointC2, 
+    radius: radius,
+    fillColor: 'green'
+});
+
+// var angle1Leg1 = pointC1 + (pointA1 - pointC1)*.5;
+// var angle1Leg2 = pointC1 + (pointB1 - pointC1)*.5;
+// var angle1 = new Path.Arc({
+//     from: angle1Leg1,
+//     through: pointC1,
+//     to: angle1Leg2,
+//     strokeColor: 'black'
+// });
+
+// var circleT1 = new Path.Circle({
+//     center: angle1Leg1,
+//     radius: radius,
+//     fillColor: 'black'
+// });
+
+
+// // making the movable circles pulse
+// function onFrame(event) {
+//     for (var i = 1; i<6; i++) {
+// 	var sinus = Math.sin(event.time * 3 + i);
+// 	circleA1.radius=sinus;
+// 	}
+// }
 
 // for moving the circleA1
 circleA1.onMouseDrag = function(event) {
     this.position = event.point;
-    circleA2.position=event.point+space;
+    //circleT1.position = (event.point + circleB1.position)/2; // swap with mid-hash
+    circleA2.position = event.point + space;
+    farside1.segments[1].point = event.point;
+    farside2.segments[1].point = event.point+space;
     triangle1.segments[0].point = event.point;
     triangle2.segments[0].point = event.point+space;
 }
@@ -66,7 +145,8 @@ circleA1.onMouseDrag = function(event) {
 // for moving the circleB1
 circleB1.onMouseDrag = function(event) {
     this.position = event.point;
-    circleB2.position=event.point+space;
+  //  circleT1.position = (event.point + circleA1.position)/2; // swap with mid-hash
+    circleB2.position = event.point + space;
     triangle1.segments[1].point = event.point;
     triangle2.segments[1].point = event.point+space;
 }
@@ -74,7 +154,9 @@ circleB1.onMouseDrag = function(event) {
 // for moving the circleC1
 circleC1.onMouseDrag = function(event) {
     this.position = event.point;
-    circleC2.position=event.point+space;
+    circleC2.position = event.point + space;
+    farside1.segments[0].point = event.point;
+    farside2.segments[0].point = event.point+space;
     triangle1.segments[2].point = event.point;
     triangle2.segments[2].point = event.point+space;
 }
@@ -87,137 +169,5 @@ circleC1.onMouseDrag = function(event) {
 
 
 
-
-
-
-
-var hitOptions = {
-	segments: true,
-	stroke: true,
-	fill: true,
-	tolerance: 5
-};
-
-var pointA = new Path.Circle(new Point(50, 200), 5);
-pointA.fillColor = 'blue'
-
-var pointB = new Path.Circle(new Point(300, 200), 5);
-pointB.fillColor = 'red'
-
-var pointC = new Path.Circle(new Point(150, 50), 5);
-pointC.fillColor = 'green'
-
-
-var path = new Path();
-path.strokeColor = 'black';
-path.add.pointA; 
-path.add.pointB; 
-path.add.pointC;
-path.closed = true;
-
-
-
-
-function onMouseDrag(event) {
-    if (path.contains(event.point)) {
-        path.position = event.point;
-    }
-}
-
-
-
-
-
-
-// EXAMPLE
-
-
-var hitOptions = { segments: true, stroke: true, fill: true, tolerance: 3 };
-
-
-//create a rectangle
-var rectangle = new paper.Rectangle(new paper.Point(200, 200), new paper.Size(100, 100)); //or (top_left, bottom_right)
-var cornerSize = new paper.Size(15, 15); //rounding of edges
-var shape = new paper.Path.RoundRectangle(rectangle, cornerSize);
-shape.strokeWidth = 3;
-shape.strokeColor = '#525252';
-shape.fillColor = '#FF6600';
-shape.name = 'shape'
-paper.view.draw();
-tool = new paper.Tool(); //Create a simple drawing tool:
-var obj
-tool.onMouseDown = function(event) //Define a mousedown and mousedrag handler
-{
-obj = null;
-var hitResult = paper.project.hitTest(event.point, hitOptions);
-
-
-if (hitResult && hitResult.type == 'fill') //state fill
-obj = hitResult.item;
-}
-tool.onMouseMove = function(event) 
-{
-var hitResult = paper.project.hitTest(event.point, hitOptions);
-paper.project.activeLayer.selected = false;
-if (hitResult && hitResult.item)
-hitResult.item.selected = true; 
-}
-tool.onMouseDrag = function(event) 
-{
-if (obj) 
-obj.position = event.point; 
-}
-tool.onMouseUp = function(event) 
-{
-}
-
-// END EXAMPLE
-
-
-
-var segment, path;
-var movePath = false;
-function onMouseDown(event) {
-	segment = path = null;
-	var hitResult = project.hitTest(event.point, hitOptions);
-	if (!hitResult)
-		return;
-
-	if (event.modifiers.shift) {
-		if (hitResult.type == 'segment') {
-			hitResult.segment.remove();
-		};
-		return;
-	}
-
-	if (hitResult) {
-		path = hitResult.item;
-		if (hitResult.type == 'segment') {
-			segment = hitResult.segment;
-		} else if (hitResult.type == 'stroke') {
-			var location = hitResult.location;
-			segment = path.insert(location.index + 1, event.point);
-			path.smooth();
-		}
-	}
-	movePath = hitResult.type == 'fill';
-	if (movePath)
-		project.activeLayer.addChild(hitResult.item);
-}
-
-function onMouseMove(event) {
-	project.activeLayer.selected = false;
-	if (event.item)
-		event.item.selected = true;
-}
-
-function onMouseDrag(event) {
-	if (segment) {
-		segment.point += event.delta;
-		path.smooth();
-	} else if (path) {
-		path.position += event.delta;
-	}
-}
 
 
